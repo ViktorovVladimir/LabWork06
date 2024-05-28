b463c99ee8c0b5bf7366ab9f2e2419e8582e0a78
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-28 22:49:32 +0300

    ex.2 was complete
    - выполнил упражнение №2 "Генерация номеров банковских счетов"

---
+++ Lab07/Starter/UniqueNumbers/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Security.Principal;
+
+class BankAccount
+{
+    long accNo;
+    decimal accBal;
+    AccountType accType;
+
+    //--.
+    public void Populate( long number, decimal balance )
+    {
+        this.accNo = number;
+        this.accBal = balance;
+        this.accType = AccountType.Checking;
+    }
+
+    //--.
+    public long Number()
+    {
+        return accNo;
+    }
+
+    //--.
+    public decimal Ballance()
+    {
+        return accBal;
+    }
+
+    //--.
+    public string Type()
+    {
+        return accType.ToString();
+    }
+
+}
